12c96586b61199b7af13863d4e41e9ce58743f43
Matt Edwards <edwardsjm01@gmail.com>
2018-10-31 20:36:13 +0000

    create 'public' directory for html

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const app = require('express')();
+const express = require('express')
+const app = express();
 const server = require('http').Server(app);
 // const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 console.log(port);
 
+app.use(express.static(__dirname + '/public'));
+
 app.use('/', (req, res) => {
-  res.send('Hello World');
+  res.sendFile('index.html', { root: __dirname + '/public' })
 });
 
 server.listen(port, () => {
